d1c3b6bd36b256b08eaef13d526cc56bda886b6c
Korneliusz Osmenda <korneliuszo@gmail.com>
2019-07-05 18:18:05 +0200

    Fixed

--- columnFormat.py
+++ columnFormat.py
@@ -10,11 +10,13 @@ Do not attempt to use this snippet in production, get a copy of python-escpos in
 """
 
 from PIL import Image, ImageOps
-import six
+import struct
 import sys
 import os
 
-def _to_column_format(im, line_height):
+ESC = b"\x1b";
+
+def _to_column_format(im):
     """
     Extract slices of an image as equal-sized blobs of column-format data.
 
@@ -22,39 +24,21 @@ def _to_column_format(im, line_height):
     :param line_height: Printed line height in dots
     """
     width_pixels, height_pixels = im.size
+    line_height = 8
     top = 0
     left = 0
-    blobs = []
     while left < width_pixels:
         remaining_pixels = width_pixels - left
         box = (left, top, left + line_height, top + height_pixels)
         slice = im.transform((line_height, height_pixels), Image.EXTENT, box)
         bytes = slice.tobytes()
-        blobs.append(bytes)
+        yield ESC + b"*" + struct.pack("<BH",1,len(bytes))
+        yield bytes
+        yield b"\r" + ESC + b"J" + struct.pack("<B",24)
         left += line_height
-    return blobs
-
-def _int_low_high(inp_number, out_bytes):
-    """ Generate multiple bytes for a number: In lower and higher parts, or more parts as needed.
-    
-    :param inp_number: Input number
-    :param out_bytes: The number of bytes to output (1 - 4).
-    """
-    max_input = (256 << (out_bytes * 8) - 1);
-    if not 1 <= out_bytes <= 4:
-        raise ValueError("Can only output 1-4 byes")
-    if not 0 <= inp_number <= max_input:
-        raise ValueError("Number too large. Can only output up to {0} in {1} byes".format(max_input, out_bytes))
-    outp = b'';
-    for _ in range(0, out_bytes):
-        outp += six.int2byte(inp_number % 256)
-        inp_number = inp_number // 256
-    return outp
 
 if __name__ == "__main__":
     # Configure
-    high_density_horizontal = True
-    high_density_vertical = True
     if len(sys.argv) > 1:
         filename = sys.argv[1]
     else:
@@ -69,18 +53,10 @@ if __name__ == "__main__":
     im = ImageOps.invert(im) # Bits are sent with 0 = white, 1 = black in ESC/POS
     im = im.convert("1") # Pure black and white
     im = im.transpose(Image.ROTATE_270).transpose(Image.FLIP_LEFT_RIGHT)
-    line_height = 3 if high_density_vertical else 1
-    blobs = _to_column_format (im, line_height * 8);
-
     # Generate ESC/POS header and print image
-    ESC = b"\x1b";
     # Height and width refer to output size here, image is rotated in memory so coordinates are swapped
-    height_pixels, width_pixels = im.size
-    density_byte = (1 if high_density_horizontal else 0) + (32 if high_density_vertical else 0);
-    header = ESC + b"*" + six.int2byte(density_byte) + _int_low_high( width_pixels, 2 );
-    
+   
     with os.fdopen(sys.stdout.fileno(), 'wb') as fp:
-        fp.write(ESC + b"3" + six.int2byte(16)); # Adjust line-feed size
-        for blob in blobs:
-            fp.write(header + blob + b"\n")
-        fp.write(ESC + b"2"); # Reset line-feed size
+        fp.write(ESC + b'@' + ESC + b'P' + ESC + b'l\x00' + b'\r' + ESC + b'Q\x00')
+        for blob in _to_column_format (im):
+            fp.write(blob)
